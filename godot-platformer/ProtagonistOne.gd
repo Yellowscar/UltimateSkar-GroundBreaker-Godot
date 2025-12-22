b14fba5b61552b50bb85f5e91d757dba6507c2ac
Yellowscar <Yellowmushroom83006@gmail.com>
2025-12-21 21:58:15 -0600

    Camera adjustment

--- godot-platformer/ProtagonistOne.gd
+++ godot-platformer/ProtagonistOne.gd
@@ -1,24 +1,33 @@
 extends CharacterBody2D
 
+#Get UltimateSkar's Camera
+@onready var UltiCamera = %UltiCamera
+
+#Basic movment variables
 var Gravity = 1400.0
 const SPEED = 200.0
 const JUMP_VELOCITY = -480.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Handle Gravity
 	if not is_on_floor():
 		velocity.y += Gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("JUMP") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		if Input.is_action_just_pressed("JUMP"):
+			velocity.y += JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("JUMP") and velocity.y < 0:
+			velocity.y *= 0.5
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# GodotNote - Get the input direction and handle the movement/deceleration.
+	# YellowNote - Handle direction, Handle Walking
+	var direction := Input.get_axis("LEFT", "RIGHT")
 	if direction:
 		velocity.x = direction * SPEED
+		UltiCamera.drag_horizontal_offset = move_toward(UltiCamera.drag_horizontal_offset, 0.15 * direction, delta * 1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
